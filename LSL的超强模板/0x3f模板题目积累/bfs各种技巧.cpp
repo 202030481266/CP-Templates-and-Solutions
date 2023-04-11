056f8059da52f52c1834748d31b6dabd7f518ee2
xiao_shulin <1258493030@qq.com>
2023-04-11 13:22:47 +0800

    update plan and code

--- LSL的超强模板/0x3f模板题目积累/bfs各种技巧.cpp
+++ LSL的超强模板/0x3f模板题目积累/bfs各种技巧.cpp
@@ -91,3 +91,68 @@ public:
     	return -1;
     }
 };
+
+
+// 难题模板3： https://leetcode.cn/problems/divide-nodes-into-the-maximum-number-of-groups/
+// bfs+二分图判定技巧（bfs可以用来进行编号）
+/*
+    1、想想简单的图——树，可以发现一定有解，并且最大的分组的数目和选择编号的起点有关系
+    2、可以证明，如果存在奇数的环，那么图是无解的。
+    3、|x-y|=1，表明编号没走一次边就会改变奇偶性质，那么如果对于奇数环来说一定存在两个奇偶相同，无解
+    4、图可解等价于二分图判定，最大的编号只能是暴力枚举+bfs，对于树而言可以使用两次dfs找到树的直径
+*/
+class Solution {
+public:
+    int magnificentSets(int n, vector<vector<int>> &edges) {
+        vector<vector<int>> g(n);
+        for (auto &e : edges) {
+            int x = e[0] - 1, y = e[1] - 1;
+            g[x].push_back(y);
+            g[y].push_back(x);
+        }
+
+        int time[n], clock = 0; 
+        memset(time, 0, sizeof(time));
+        auto bfs = [&](int start) -> int {
+            int depth = 0;
+            time[start] = ++clock;
+            vector<int> q = {start};
+            while (!q.empty()) {
+                vector<int> nxt;
+                for (int x : q)
+                    for (int y : g[x])
+                        if (time[y] != clock) { 
+                            time[y] = clock;
+                            nxt.push_back(y);
+                        }
+                q = move(nxt);
+                ++depth;
+            }
+            return depth;
+        };
+
+        int8_t color[n]; memset(color, 0, sizeof(color));
+        vector<int> nodes;
+        function<bool(int, int8_t)> is_bipartite = [&](int x, int8_t c) -> bool { 
+            nodes.push_back(x);
+            color[x] = c;
+            for (int y : g[x])
+                if (color[y] == c || color[y] == 0 && !is_bipartite(y, -c))
+                    return false;
+            return true;
+        };
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (color[i]) continue;
+            nodes.clear();
+            if (!is_bipartite(i, 1)) return -1; 
+            int max_depth = 0;
+            for (int x : nodes) 
+                max_depth = max(max_depth, bfs(x));
+            ans += max_depth;
+        }
+        return ans;
+    }
+};
+
