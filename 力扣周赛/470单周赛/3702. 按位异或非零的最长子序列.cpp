79dcfc5ecb683bee49926f9566f5530ff234adc7
xiao_shulin <1258493030@qq.com>
2025-10-06 03:07:22 +0800

    [update] 一个显而易见的更快的解法

--- 力扣周赛/470单周赛/3702. 按位异或非零的最长子序列.cpp
+++ 力扣周赛/470单周赛/3702. 按位异或非零的最长子序列.cpp
@@ -9,26 +9,12 @@ static constexpr int inf = 0x3f3f3f3f;
 class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
-        // 枚举不同的位置就可以了，注意需要判断一个的情况
-        vector<int> cnt(32);
+        int s = 0, n = nums.size(), all_zero = 1;
         for (int v : nums) {
-            for (int i = 0; i < 32; ++i) {
-                cnt[i] += (v >> i) & 1;
-            }
+            s ^= v;
+            if (v) all_zero = 0;
         }
-        int ans = 0, n = nums.size();
-        for (int v : nums) {
-            if (v > 0) {
-                ans = 1;
-                break;
-            }
-        }
-        if (ans == 0) return ans;
-        for (int i = 0; i < 32; ++i) {
-            int x = cnt[i], y = n - x;
-            if (x & 1) ans = max(ans, n);
-            else if (x > 1) ans = max(ans, y + x - 1);
-        }
-        return ans;
+        if (all_zero) return 0;
+        return s > 0 ? n : n - 1;
     }
 };
