1fc8eff82877fb5c1dcafbe4e22a18dc30697cac
xiao_shulin <1258493030@qq.com>
2025-12-22 08:51:06 +0800

    [add] add lc contest folder

---
+++ 力扣周赛/481单周赛/3786. 树组的交互代价总和.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+static constexpr int MAXN = 200005;
+static constexpr int MOD = 1e9 + 7;
+static constexpr int INF = 0x3f3f3f3f;
+
+class Solution {
+public:
+    long long interactionCosts(int n, vector<vector<int>>& edges, vector<int>& group) {
+        
+    }
+};
