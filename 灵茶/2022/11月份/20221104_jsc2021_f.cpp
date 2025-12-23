f9024c84cf4e8f796ddb103e975e42543445c5c7
xiao_shulin <1258493030@qq.com>
2025-12-23 09:55:04 +0800

    [add] drink tea

---
+++ 灵茶/2022/11月份/20221104_jsc2021_f.cpp
@@ -0,0 +1,531 @@
+// Problem: F - Max Matrix
+// Contest: AtCoder - Japanese Student Championship 2021
+// URL: https://atcoder.jp/contests/jsc2021/tasks/jsc2021_f
+// Memory Limit: 1024 MB
+// Time Limit: 3000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#ifdef _MSC_VER
+#include "pch.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef std::pair<int, int> pii;
+typedef std::vector<int> vi;
+typedef std::vector<vector<int>> vii;
+typedef std::vector<ll> vl;
+typedef std::vector<vector<ll>> vll;
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char* x) { cerr << '\"' << x << '\"'; }
+void __print(const string& x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template<typename T, typename V>
+void __print(const pair<T, V>& x) { cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}'; }
+template<typename T>
+void __print(const T& x) { int f = 0; cerr << '{'; for (auto& i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}"; }
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v) { __print(t); if (sizeof...(v)) cerr << ", "; _print(v...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+namespace FastIO {
+	const int BUFSIZE = 1 << 20; // 1MB缓冲区
+	char ibuf[BUFSIZE], obuf[BUFSIZE];
+	char* p1 = ibuf, * p2 = ibuf;
+	char* p3 = obuf;
+
+	// 读取单个字符
+	inline char gc() {
+		if (p1 == p2) {
+			p1 = ibuf;
+			p2 = ibuf + fread(ibuf, 1, BUFSIZE, stdin);
+			if (p1 == p2) return EOF;
+		}
+		return *p1++;
+	}
+
+	// 输出单个字符
+	inline void pc(char c) {
+		if (p3 - obuf == BUFSIZE) {
+			fwrite(obuf, 1, BUFSIZE, stdout);
+			p3 = obuf;
+		}
+		*p3++ = c;
+	}
+
+	// 刷新输出缓冲区
+	inline void flush() {
+		fwrite(obuf, 1, p3 - obuf, stdout);
+		p3 = obuf;
+	}
+
+	// 输出字符串（提前声明，供其他函数使用）
+	inline void write(const string& s) {
+		for (char c : s) pc(c);
+	}
+
+	// 输出C风格字符串
+	inline void write(const char* s) {
+		while (*s) pc(*s++);
+	}
+
+	// 读取整数（支持负数）
+	template<typename T>
+	inline typename enable_if<is_integral<T>::value, bool>::type read(T& x) {
+		x = 0;
+		char c = gc();
+		bool neg = false;
+
+		// 跳过空白字符
+		while (c != EOF && !isdigit(c) && c != '-') c = gc();
+		if (c == EOF) return false;
+
+		// 处理负号
+		if (c == '-') {
+			neg = true;
+			c = gc();
+		}
+
+		// 读取数字
+		while (isdigit(c)) {
+			x = x * 10 + (c - '0');
+			c = gc();
+		}
+
+		if (neg) x = -x;
+		return true;
+	}
+
+	// 读取浮点数
+	template<typename T>
+	inline typename enable_if<is_floating_point<T>::value, bool>::type read(T& x) {
+		x = 0;
+		char c = gc();
+		bool neg = false;
+
+		// 跳过空白字符
+		while (c != EOF && !isdigit(c) && c != '-' && c != '.') c = gc();
+		if (c == EOF) return false;
+
+		// 处理负号
+		if (c == '-') {
+			neg = true;
+			c = gc();
+		}
+
+		// 读取整数部分
+		while (isdigit(c)) {
+			x = x * 10 + (c - '0');
+			c = gc();
+		}
+
+		// 读取小数部分
+		if (c == '.') {
+			c = gc();
+			T frac = 0.1;
+			while (isdigit(c)) {
+				x += (c - '0') * frac;
+				frac *= 0.1;
+				c = gc();
+			}
+		}
+
+		// 处理科学计数法
+		if (c == 'e' || c == 'E') {
+			c = gc();
+			int exp = 0;
+			bool exp_neg = false;
+			if (c == '-') {
+				exp_neg = true;
+				c = gc();
+			}
+			else if (c == '+') {
+				c = gc();
+			}
+			while (isdigit(c)) {
+				exp = exp * 10 + (c - '0');
+				c = gc();
+			}
+			if (exp_neg) exp = -exp;
+			x *= pow(10, exp);
+		}
+
+		if (neg) x = -x;
+		return true;
+	}
+
+	// 输出整数
+	template<typename T>
+	inline typename enable_if<is_integral<T>::value>::type write(T x) {
+		static char stk[30];
+		int top = 0;
+
+		if (x < 0) {
+			pc('-');
+			x = -x;
+		}
+
+		if (x == 0) {
+			pc('0');
+			return;
+		}
+
+		while (x) {
+			stk[top++] = '0' + (x % 10);
+			x /= 10;
+		}
+
+		while (top) {
+			pc(stk[--top]);
+		}
+	}
+
+	// 输出浮点数（默认6位小数）
+	template<typename T>
+	inline typename enable_if<is_floating_point<T>::value>::type write(T x, int precision = 6) {
+		if (x < 0) {
+			pc('-');
+			x = -x;
+		}
+
+		// 处理特殊值
+		if (isnan(x)) {
+			write("nan");
+			return;
+		}
+		if (isinf(x)) {
+			write("inf");
+			return;
+		}
+
+		// 四舍五入
+		T round_val = 0.5;
+		for (int i = 0; i < precision; i++) {
+			round_val /= 10;
+		}
+		x += round_val;
+
+		// 输出整数部分
+		ll int_part = (ll)x;
+		write(int_part);
+
+		// 输出小数点和小数部分
+		if (precision > 0) {
+			pc('.');
+			x -= int_part;
+			for (int i = 0; i < precision; i++) {
+				x *= 10;
+				int digit = (int)x;
+				pc('0' + digit);
+				x -= digit;
+			}
+		}
+	}
+
+	// 输出整数并换行
+	template<typename T>
+	inline typename enable_if<is_integral<T>::value>::type writeln(T x) {
+		write(x);
+		pc('\n');
+	}
+
+	// 输出整数和分隔符
+	template<typename T>
+	inline typename enable_if<is_integral<T>::value>::type write(T x, char sep) {
+		write(x);
+		pc(sep);
+	}
+
+	// 输出浮点数并换行
+	template<typename T>
+	inline typename enable_if<is_floating_point<T>::value>::type writeln(T x, int precision = 6) {
+		write(x, precision);
+		pc('\n');
+	}
+
+	// 输出浮点数并且和分隔符
+	template<typename T>
+	inline typename enable_if<is_floating_point<T>::value>::type write(T x, char sep) {
+		write(x);
+		pc(sep);
+	}
+
+	// 读取字符串（读到空白字符为止）
+	inline bool read(string& s) {
+		s.clear();
+		char c = gc();
+
+		// 跳过空白字符
+		while (c != EOF && isspace(c)) c = gc();
+		if (c == EOF) return false;
+
+		// 读取非空白字符
+		while (c != EOF && !isspace(c)) {
+			s += c;
+			c = gc();
+		}
+		return true;
+	}
+
+	// 读取整行
+	inline bool getline(string& s) {
+		s.clear();
+		char c = gc();
+
+		if (c == EOF) return false;
+
+		while (c != EOF && c != '\n') {
+			s += c;
+			c = gc();
+		}
+		return true;
+	}
+
+	// 输出字符串并换行
+	inline void writeln(const string& s) {
+		write(s);
+		pc('\n');
+	}
+
+	// 读取单个非空白字符
+	inline char readchar() {
+		char c = gc();
+		while (c != EOF && isspace(c)) c = gc();
+		return c;
+	}
+
+	// 程序结束时自动刷新缓冲区
+	struct AutoFlush {
+		~AutoFlush() { flush(); }
+	} auto_flush;
+}
+using namespace FastIO;
+
+static constexpr int MAXN = 200005;
+static constexpr int INF = 0x3f3f3f3f;
+static constexpr ll LLINF = 0x3f3f3f3f3f3f3f3f;
+static constexpr int MOD = 1e9 + 7;
+
+
+#include <cassert>
+#include <vector>
+
+
+#include <cassert>
+#include <numeric>
+#include <type_traits>
+
+namespace atcoder {
+
+namespace internal {
+
+#ifndef _MSC_VER
+template <class T>
+using is_signed_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value ||
+                                  std::is_same<T, __int128>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using is_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __uint128_t>::value ||
+                                  std::is_same<T, unsigned __int128>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using make_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value,
+                              __uint128_t,
+                              unsigned __int128>;
+
+template <class T>
+using is_integral = typename std::conditional<std::is_integral<T>::value ||
+                                                  is_signed_int128<T>::value ||
+                                                  is_unsigned_int128<T>::value,
+                                              std::true_type,
+                                              std::false_type>::type;
+
+template <class T>
+using is_signed_int = typename std::conditional<(is_integral<T>::value &&
+                                                 std::is_signed<T>::value) ||
+                                                    is_signed_int128<T>::value,
+                                                std::true_type,
+                                                std::false_type>::type;
+
+template <class T>
+using is_unsigned_int =
+    typename std::conditional<(is_integral<T>::value &&
+                               std::is_unsigned<T>::value) ||
+                                  is_unsigned_int128<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using to_unsigned = typename std::conditional<
+    is_signed_int128<T>::value,
+    make_unsigned_int128<T>,
+    typename std::conditional<std::is_signed<T>::value,
+                              std::make_unsigned<T>,
+                              std::common_type<T>>::type>::type;
+
+#else
+
+template <class T> using is_integral = typename std::is_integral<T>;
+
+template <class T>
+using is_signed_int =
+    typename std::conditional<is_integral<T>::value && std::is_signed<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using is_unsigned_int =
+    typename std::conditional<is_integral<T>::value &&
+                                  std::is_unsigned<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using to_unsigned = typename std::conditional<is_signed_int<T>::value,
+                                              std::make_unsigned<T>,
+                                              std::common_type<T>>::type;
+
+#endif
+
+template <class T>
+using is_signed_int_t = std::enable_if_t<is_signed_int<T>::value>;
+
+template <class T>
+using is_unsigned_int_t = std::enable_if_t<is_unsigned_int<T>::value>;
+
+template <class T> using to_unsigned_t = typename to_unsigned<T>::type;
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+namespace atcoder {
+
+template <class T> struct fenwick_tree {
+    using U = internal::to_unsigned_t<T>;
+
+  public:
+    fenwick_tree() : _n(0) {}
+    explicit fenwick_tree(int n) : _n(n), data(n) {}
+
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        p++;
+        while (p <= _n) {
+            data[p - 1] += U(x);
+            p += p & -p;
+        }
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+  private:
+    int _n;
+    std::vector<U> data;
+
+    U sum(int r) {
+        U s = 0;
+        while (r > 0) {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+};
+
+}  // namespace atcoder
+
+using namespace atcoder;
+
+void solve() {
+	int n, m, q;
+	read(n);
+	read(m);
+	read(q);
+	vector<array<int, 3>> qs(q);
+	vector<pii> a;
+	vi rk(q), num;
+	a.emplace_back(0, -1);
+	for (int i = 0; i < q; ++i) {
+		read(qs[i][0]);
+		read(qs[i][1]);
+		read(qs[i][2]);
+		a.emplace_back(qs[i][2], i);
+	}
+	ranges::sort(a);
+	int cur = 0;
+	num.push_back(0);
+	for (int i = 1; i <= q; ++i) {
+		if (a[i].first != a[i - 1].first) {
+			++cur;
+			num.push_back(a[i].first);
+		}
+		rk[a[i].second] = cur;
+	}
+	vector<fenwick_tree<int>> sz(2, fenwick_tree<int>(cur + 1));
+	vector<fenwick_tree<ll>> tree(2, fenwick_tree<ll>(cur + 1));
+	vii arr(2);
+	arr[0].resize(n + 1);
+	arr[1].resize(m + 1);
+	sz[0].add(0, n);
+	sz[1].add(0, m);
+	ll ans = 0;
+	auto cal = [&](int k, int val) -> ll {
+		int ge_sz = sz[k ^ 1].sum(0, val);
+		ll res = 1ll * num[val] * ge_sz + tree[k ^ 1].sum(0, cur + 1) - tree[k ^ 1].sum(0, val); // [val, cur]
+		return res;
+	};
+	for (int i = 0; i < q; ++i) {
+		int k = qs[i][0] - 1, pre = arr[k][qs[i][1]];
+		ll new_val = cal(k, rk[i]), old_val = cal(k, pre);
+		ans += new_val - old_val;
+		// update the array
+		sz[k].add(pre, -1);
+		tree[k].add(pre, -num[pre]);
+		arr[k][qs[i][1]] = rk[i];
+		sz[k].add(rk[i], 1);
+		tree[k].add(rk[i], num[rk[i]]);
+		writeln(ans);
+	}
+}
+
+
+int main() {
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+}
