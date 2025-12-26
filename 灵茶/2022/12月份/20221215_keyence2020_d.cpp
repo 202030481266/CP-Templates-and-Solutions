f6e109cfd83635eafec2b5457be02e5057ae6f9f
xiao_shulin <1258493030@qq.com>
2025-12-26 09:40:55 +0800

    [add] drink tea

---
+++ 灵茶/2022/12月份/20221215_keyence2020_d.cpp
@@ -0,0 +1,8 @@
+// Problem: D - Swap and Flip
+// Contest: AtCoder - Keyence Programming Contest 2020
+// URL: https://atcoder.jp/contests/keyence2020/tasks/keyence2020_d
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
