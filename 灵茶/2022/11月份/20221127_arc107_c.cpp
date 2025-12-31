d3a85b112b48fcec8126ca1722100e7207ef96f5
xiao_shulin <1258493030@qq.com>
2025-12-31 09:57:51 +0800

    [add] deink tea

---
+++ 灵茶/2022/11月份/20221127_arc107_c.cpp
@@ -0,0 +1,8 @@
+// Problem: C - Shuffle Permutation
+// Contest: AtCoder - AtCoder Regular Contest 107
+// URL: https://atcoder.jp/contests/arc107/tasks/arc107_c
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
