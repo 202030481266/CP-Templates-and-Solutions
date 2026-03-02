aaca01028cda9d1177f7280b0406579953ac3358
criscuolosubidu <criscuolosubidu@gmail.com>
2026-03-02 10:45:09 +0800

    [add] update tea

--- tea-0x3f/2026/2月份/20260228_1592E.cpp
+++ tea-0x3f/2026/2月份/20260228_1592E.cpp
@@ -20,11 +20,72 @@ static constexpr int INF = 0x3f3f3f3f;
 
 int n, arr[MAXN], sum[MAXN];
 
+static constexpr int MAX_BIT = 20;
+static constexpr int MAX_NODE = MAXN << 2;
+
+int tree[MAX_NODE][2], tot = 0;
+int ptr[MAX_NODE];
+vi idx[MAX_NODE];
+
+int alloc() {
+    int x = ++tot;
+    tree[x][0] = tree[x][1] = 0;
+    return x;
+}
+
+void reset() {
+    for (int i = 1; i <= tot; ++i) {
+        tree[i][0] = tree[i][1] = 0;
+        idx[i].clear();
+        ptr[i] = 0;
+    }
+    tot = 0;
+}
+
+int root;
+
+void insert(int x, int root, int p) {
+    int u = root;
+    for (int i = MAX_BIT - 1; i >= 0; --i) {
+        int b = x >> i & 1;
+        if (!tree[u][b]) tree[u][b] = alloc();
+        u = tree[u][b];
+        idx[u].push_back(p);
+    }
+}
+
+int pos[MAX_BIT];
+
 void solve() {
     cin >> n;
     for (int i = 1; i <= n; ++i) cin >> arr[i];
     for (int i = 1; i <= n; ++i) sum[i] = arr[i] ^ sum[i - 1];
-    
+    int ans = 0;
+    auto cal = [&](int st) -> void {
+        reset();
+        root = alloc();
+        memset(pos, 0, sizeof(pos));
+        for (int i = st; i <= n; ++i) {
+            for (int j = MAX_BIT - 1; j >= 0; --j) {
+                if (!(arr[i] >> j & 1)) pos[j] = i;
+            }
+            if ((i & 1) != (st & 1)) continue;
+            int u = root;
+            for (int j = MAX_BIT - 1; j >= 0; --j) {
+                int b = sum[i] >> j & 1;
+                if (!tree[u][b]) break;
+                u = tree[u][b];
+                while (ptr[u] < idx[u].size() && idx[u][ptr[u]] < pos[j]) ++ptr[u];
+                if (ptr[u] < idx[u].size() && ans < i - idx[u][ptr[u]]) {
+                    ans = i - idx[u][ptr[u]];
+                }
+            }
+            insert(sum[i], root, i);
+        }
+    };
+    cal(0);
+    cal(1);
+    cout << ans << '\n';
 }
 
 int main() {
