6597bf717dd3d81e996418d1a455cbdccf35c97d
xiao_shulin <1258493030@qq.com>
2026-04-02 01:45:01 +0800

    [add] add cf2400~3000 hard problems

---
+++ tea-0x3f/cf2400~3000/1107F.cpp
@@ -0,0 +1,154 @@
+// Problem: F. Vasya and Endless Credits
+// Contest: Codeforces - Educational Codeforces Round 59 (Rated for Div. 2)
+// URL: https://codeforces.com/problemset/problem/1107/F
+// Memory Limit: 256 MB
+// Time Limit: 3000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#ifdef _MSC_VER
+#include "pch.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+#include <format>       // std::format
+#include <concepts>     // concepts
+#include <ranges>       // std::ranges
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using uint = unsigned int;
+using ld = long double;
+
+template<typename T, typename U = T>
+using pr = pair<T, U>;
+
+template<typename T>
+using vec = vector<T>;
+
+using pii = pr<int>;
+using pll = pr<ll>;
+using vi = vec<int>;
+using vl = vec<ll>;
+using vii = vec<vi>;
+using vll = vec<vl>;
+using vp = vec<pii>;
+using vpp = vec<vp>;
+
+template<typename T>
+concept Printable = requires(T x) { cerr << x; };
+
+template<typename T>
+concept PairLike = requires(T x) { x.first; x.second; };
+
+template<typename T>
+concept Iterable = ranges::range<T> && !PairLike<T> && !is_convertible_v<T, string_view>;
+
+template<typename T> void __print(const T&);
+
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+void __print(const char* x) { cerr << '"' << x << '"'; }
+void __print(const string& x) { cerr << '"' << x << '"'; }
+void __print(string_view x) { cerr << '"' << x << '"'; }
+template<Printable T>
+	requires (!PairLike<T> && !Iterable<T>
+&& !is_same_v<T, char>
+&& !is_same_v<T, bool>
+&& !is_same_v<T, string>
+&& !is_same_v<T, string_view>
+&& !is_convertible_v<T, const char*>)
+void __print(const T& x) { cerr << x; }
+
+template<PairLike T>
+void __print(const T& x) {
+	cerr << '{';
+	__print(x.first);
+	cerr << ", ";
+	__print(x.second);
+	cerr << '}';
+}
+
+template<typename... Ts>
+void __print(const tuple<Ts...>& t) {
+	cerr << '(';
+	apply([&]<typename... Args>(const Args&... args) {
+		size_t n = 0;
+		((cerr << (n++ ? ", " : ""), __print(args)), ...);
+	}, t);
+	cerr << ')';
+}
+
+template<Iterable T>
+void __print(const T& x) {
+	cerr << '{';
+	bool first = true;
+	for (const auto& i : x) {
+		if (!first) cerr << ", ";
+		__print(i);
+		first = false;
+	}
+	cerr << '}';
+}
+
+template<typename T>
+void __print(const optional<T>& x) {
+	if (x) { cerr << "Some("; __print(*x); cerr << ')'; }
+	else     cerr << "None";
+}
+
+void _print() { cerr << "]\n"; }
+
+template<typename T, typename... V>
+void _print(T&& t, V&&... v) {
+	__print(t);
+	if constexpr (sizeof...(v) > 0) cerr << ", ";
+	_print(forward<V>(v)...);
+}
+
+#ifndef ONLINE_JUDGE
+#   define debug(...) \
+        cerr << std::format("[{}:{}] [{}] = [", __FILE_NAME__, __LINE__, #__VA_ARGS__); \
+        _print(__VA_ARGS__)
+#else
+#   define debug(...)
+#endif
+
+inline constexpr int  MAXN = 500'005;
+inline constexpr int  INF = 0x3f3f3f3f;
+inline constexpr ll   LLINF = 0x3f3f3f3f3f3f3f3fLL;
+inline constexpr int  MOD = 1'000'000'007;
+inline constexpr int  MAXB = 30;
+
+inline constexpr int  INT_INF = numeric_limits<int>::max() / 2;
+inline constexpr ll   LL_INF = numeric_limits<ll>::max() / 2;
+
+// guess1: 在第c个月贷款，然后收益曲线就是 y = -bi * (x - ci) + ai = -bi * x + bi * ci + ai
+// guess2: 最好的方法肯定是连续贷款，中间不会有空缺，不然肯定会有更加好的方案，而且最后的答案肯定也不会超过n，拿完走人了啊
+// guess3: 对于第 i 个物品而言， 决策需要的东西：当前已经有的负债表状态，关键是怎么表示？
+// guess4: 最佳方案中绝对不会存在一个物品 i，它对于整体的贡献是负的。（我为什么要搬起石头砸自己的脚？）
+
+int n;
+int a[503], b[503], k[503];
+
+void solve() {
+	cin >> n;
+	for (int i = 1; i <= n; ++i) cin >> a[i] >> b[i] >> k[i];
+	// 枚举其中的天数
+	for (int day = 0; day <= n; ++day) {
+		// dp[i][x] = max(dp[i - 1][x], dp[i - 1][x - bi] + ai)
+
+	}
+}
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+}
