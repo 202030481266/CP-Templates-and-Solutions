b10042009cedb04f0bf7bd715ac85384b54f47ee
xiao_shulin <1258493030@qq.com>
2026-04-09 13:12:39 +0800

    [update] update tea

--- tea-0x3f/cf2400~3000/1672H.cpp
+++ tea-0x3f/cf2400~3000/1672H.cpp
@@ -120,45 +120,34 @@ inline constexpr ll   LL_INF = numeric_limits<ll>::max() / 2;
 
 
 // guess1: 1s的时间，说明存在非常高效的方法解决，甚至不带log
-// guess2: 建模为一个01block的模型，比方说 0001101101 -> [3,2,1,2,1,1]
-// guess3: 0相当于往下走（因为0如果要匹配后面的1，必须前面的0消除掉），1相当于往上走（匹配前面的0，那么前面的0肯定要消除掉）
-// guess3: 感觉还是栈的关系，这个计数有点巧妙了，如何快速统计是一个难题，应该是和波峰有关系
+// guess2: 0相当于往下走（因为0如果要匹配后面的1，必须前面的0消除掉），1相当于往上走（匹配前面的0，那么前面的0肯定要消除掉）
+// guess3: 找到 [l,r] 区间中所有小于 h[l] 的点上升以及大于 h[l] 的下降，它们的和 + 1
+// guess4: 找到上下起伏的总的波动数/2?
 
 int arr[MAXN];
-int belong[MAXN], bl[MAXN], br[MAXN];
+int sum[MAXN];
+int height[MAXN];
 
 void solve() {
 	int n, m;
 	cin >> n >> m;
-	string data;
-	cin >> data;
-	for (int i = 1; i <= n; ++i) arr[i] = data[i - 1] - '0';
-	int p = 1, num = 0;
-	vi block;
-	vi sum{ 0 };
-	while (p <= n) {
-		int i = p + 1;
-		while (i <= n && arr[i] == arr[p]) ++i;
-		++num;
-		for (int j = p; j < i; ++j) belong[j] = num;
-		bl[num] = p;	
-		br[num] = i - 1;
-		block.push_back(i - p);
-		sum.push_back(sum.back() + i - p);
-		p = i;
+	char c;
+	for (int i = 1; i <= n; ++i) {
+		cin >> c;
+		arr[i] = c - '0';
+		sum[i] = sum[i - 1];
+		height[i] = height[i - 1];
+		if (i > 1 && arr[i] == arr[i - 1]) {
+			if (arr[i]) ++height[i];
+			else --height[i];
+			++sum[i];
+		}
 	}
 	for (int t = 0, l, r; t < m; ++t) {
 		cin >> l >> r;
-		if (belong[l] == belong[r]) cout << r - l + 1 << '\n';
-		else {
-			int left_num = br[belong[l]] - l + 1;
-			int right_num = r - bl[belong[r]] + 1;
-			if (belong[r] == belong[l] + 1) {
-				cout << (right_num > left_num ? right_num : left_num) << '\n';
-			}
-			else {
-			}
-		}
+		int s = sum[r] - sum[l - 1];
+		s += abs(height[r] - height[l]);
+		cout << (s / 2 + 1) << '\n';
 	}
 }
 
