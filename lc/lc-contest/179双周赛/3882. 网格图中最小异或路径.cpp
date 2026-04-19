e009837883f68889d1eb5c966d7eb40b01162456
xiao_shulin <1258493030@qq.com>
2026-04-19 17:24:12 +0800

    [add] update code

---
+++ lc/lc-contest/179双周赛/3882. 网格图中最小异或路径.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<ll>;
+using vll = vector<vector<ll>>;
+static constexpr int MAXN = 200005;
+static constexpr int MOD = 1e9 + 7;
+static constexpr int INF = 0x3f3f3f3f;
+
+int dp[1002][1002];
+
+class Solution {
+public:
+    int minCost(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        
+    }
+};
