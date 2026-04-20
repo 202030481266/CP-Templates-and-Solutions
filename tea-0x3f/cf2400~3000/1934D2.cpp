207bd1529aa3df724c04a4c437342738f36a6344
xiao_shulin <1258493030@qq.com>
2026-04-20 22:02:31 +0800

    [add] drink tea

---
+++ tea-0x3f/cf2400~3000/1934D2.cpp
@@ -0,0 +1,169 @@
+// Problem: D2. XOR Break — Game Version
+// Contest: Codeforces - Codeforces Round 931 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/1934/D2
+// Memory Limit: 256 MB
+// Time Limit: 3000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#ifdef _MSC_VER
+#include "pch.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+#include <format>       // std::format
+#include <concepts>     // concepts
+#include <ranges>       // std::ranges
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using uint = unsigned int;
+using ld = long double;
+
+template<typename T, typename U = T>
+using pr = pair<T, U>;
+
+template<typename T>
+using vec = vector<T>;
+
+using pii = pr<int>;
+using pll = pr<ll>;
+using vi = vec<int>;
+using vl = vec<ll>;
+using vii = vec<vi>;
+using vll = vec<vl>;
+using vp = vec<pii>;
+using vpp = vec<vp>;
+
+template<typename T>
+concept Printable = requires(T x) { cerr << x; };
+
+template<typename T>
+concept PairLike = requires(T x) { x.first; x.second; };
+
+template<typename T>
+concept Iterable = ranges::range<T> && !PairLike<T> && !is_convertible_v<T, string_view>;
+
+template<typename T> void __print(const T&);
+
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+void __print(const char* x) { cerr << '"' << x << '"'; }
+void __print(const string& x) { cerr << '"' << x << '"'; }
+void __print(string_view x) { cerr << '"' << x << '"'; }
+template<Printable T>
+	requires (!PairLike<T> && !Iterable<T>
+&& !is_same_v<T, char>
+&& !is_same_v<T, bool>
+&& !is_same_v<T, string>
+&& !is_same_v<T, string_view>
+&& !is_convertible_v<T, const char*>)
+void __print(const T& x) { cerr << x; }
+
+template<PairLike T>
+void __print(const T& x) {
+	cerr << '{';
+	__print(x.first);
+	cerr << ", ";
+	__print(x.second);
+	cerr << '}';
+}
+
+template<typename... Ts>
+void __print(const tuple<Ts...>& t) {
+	cerr << '(';
+	apply([&]<typename... Args>(const Args&... args) {
+		size_t n = 0;
+		((cerr << (n++ ? ", " : ""), __print(args)), ...);
+	}, t);
+	cerr << ')';
+}
+
+template<Iterable T>
+void __print(const T& x) {
+	cerr << '{';
+	bool first = true;
+	for (const auto& i : x) {
+		if (!first) cerr << ", ";
+		__print(i);
+		first = false;
+	}
+	cerr << '}';
+}
+
+template<typename T>
+void __print(const optional<T>& x) {
+	if (x) { cerr << "Some("; __print(*x); cerr << ')'; }
+	else     cerr << "None";
+}
+
+void _print() { cerr << "]\n"; }
+
+template<typename T, typename... V>
+void _print(T&& t, V&&... v) {
+	__print(t);
+	if constexpr (sizeof...(v) > 0) cerr << ", ";
+	_print(forward<V>(v)...);
+}
+
+#ifndef ONLINE_JUDGE
+#   define debug(...) \
+        cerr << std::format("[{}:{}] [{}] = [", __FILE_NAME__, __LINE__, #__VA_ARGS__); \
+        _print(__VA_ARGS__)
+#else
+#   define debug(...)
+#endif
+
+inline constexpr int  MAXN = 500'005;
+inline constexpr int  INF = 0x3f3f3f3f;
+inline constexpr ll   LLINF = 0x3f3f3f3f3f3f3f3fLL;
+inline constexpr int  MOD = 1'000'000'007;
+inline constexpr int  MAXB = 30;
+
+inline constexpr int  INT_INF = numeric_limits<int>::max() / 2;
+inline constexpr ll   LL_INF = numeric_limits<ll>::max() / 2;
+
+
+// guess1: 和 popcount(n) 有关，odd+even->odd, even+even/odd+odd->even
+// guess2: 无论怎么分解odd都会分解出来一个偶数和奇数，对手选择偶数必赢，even无论怎么分解都会让对手选择一个odd，对手必输
+
+void output(ull x, ull y) {
+	cout << format("{} {}", x, y) << endl;
+}
+
+void solve() {
+	ull n;
+	cin >> n;
+	int b = popcount(n);
+	cout << (b & 1 ? "second" : "first") << endl;
+	ull x, y;
+	int turn = ~b & 1;
+	while (true) {
+		if (turn) { // 肯定是偶数
+			int h = bit_width(n);
+			x = 1ULL << h - 1;
+			y = n - x;
+			output(x, y);
+		}
+		else {
+			cin >> x >> y;
+			if (x == 0 && y == 0) break;
+			if (popcount(x) & 1) n = y;
+			else n = x;
+		}
+		turn ^= 1;
+	}
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(&cout);
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+	return 0;
+}
