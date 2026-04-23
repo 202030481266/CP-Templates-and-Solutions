dd6903f4ddfcdb57df449ef61391ac5754917a1e
xiao_shulin <1258493030@qq.com>
2026-04-24 01:02:03 +0800

    [add] drink hard tea

---
+++ tea-0x3f/cf2400~3000/1863F.cpp
@@ -0,0 +1,163 @@
+#ifdef _MSC_VER
+#include "pch.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+#include <format>       // std::format
+#include <concepts>     // concepts
+#include <ranges>       // std::ranges
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using uint = unsigned int;
+using ld = long double;
+
+template<typename T, typename U = T>
+using pr = pair<T, U>;
+
+template<typename T>
+using vec = vector<T>;
+
+using pii = pr<int>;
+using pll = pr<ll>;
+using vi = vec<int>;
+using vl = vec<ll>;
+using vii = vec<vi>;
+using vll = vec<vl>;
+using vp = vec<pii>;
+using vpp = vec<vp>;
+
+template<typename T>
+concept Printable = requires(T x) { cerr << x; };
+
+template<typename T>
+concept PairLike = requires(T x) { x.first; x.second; };
+
+template<typename T>
+concept Iterable = ranges::range<T> && !PairLike<T> && !is_convertible_v<T, string_view>;
+
+template<typename T> void __print(const T&);
+
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+void __print(const char* x) { cerr << '"' << x << '"'; }
+void __print(const string& x) { cerr << '"' << x << '"'; }
+void __print(string_view x) { cerr << '"' << x << '"'; }
+template<Printable T>
+	requires (!PairLike<T> && !Iterable<T>
+&& !is_same_v<T, char>
+&& !is_same_v<T, bool>
+&& !is_same_v<T, string>
+&& !is_same_v<T, string_view>
+&& !is_convertible_v<T, const char*>)
+void __print(const T& x) { cerr << x; }
+
+template<PairLike T>
+void __print(const T& x) {
+	cerr << '{';
+	__print(x.first);
+	cerr << ", ";
+	__print(x.second);
+	cerr << '}';
+}
+
+template<typename... Ts>
+void __print(const tuple<Ts...>& t) {
+	cerr << '(';
+	apply([&]<typename... Args>(const Args&... args) {
+		size_t n = 0;
+		((cerr << (n++ ? ", " : ""), __print(args)), ...);
+	}, t);
+	cerr << ')';
+}
+
+template<Iterable T>
+void __print(const T& x) {
+	cerr << '{';
+	bool first = true;
+	for (const auto& i : x) {
+		if (!first) cerr << ", ";
+		__print(i);
+		first = false;
+	}
+	cerr << '}';
+}
+
+template<typename T>
+void __print(const optional<T>& x) {
+	if (x) { cerr << "Some("; __print(*x); cerr << ')'; }
+	else     cerr << "None";
+}
+
+void _print() { cerr << "]\n"; }
+
+template<typename T, typename... V>
+void _print(T&& t, V&&... v) {
+	__print(t);
+	if constexpr (sizeof...(v) > 0) cerr << ", ";
+	_print(forward<V>(v)...);
+}
+
+#ifndef ONLINE_JUDGE
+#   define debug(...) \
+        cerr << std::format("[{}:{}] [{}] = [", __FILE_NAME__, __LINE__, #__VA_ARGS__); \
+        _print(__VA_ARGS__)
+#else
+#   define debug(...)
+#endif
+
+inline constexpr int  MAXN = 200'005;
+inline constexpr int  INF = 0x3f3f3f3f;
+inline constexpr ll   LLINF = 0x3f3f3f3f3f3f3f3fLL;
+inline constexpr int  MOD = 1'000'000'007;
+inline constexpr int  MAXB = 30;
+
+inline constexpr int  INT_INF = numeric_limits<int>::max() / 2;
+inline constexpr ll   LL_INF = numeric_limits<ll>::max() / 2;
+
+inline constexpr int N = 10001;
+using bs = bitset<N>;
+
+void solve() {
+	int n;
+	cin >> n;
+	vl arr(n + 1), sum(n + 1);
+	for (int i = 1; i <= n; ++i) cin >> arr[i];
+	for (int i = 1; i <= n; ++i) sum[i] = sum[i - 1] ^ arr[i];
+	vec<bs> dp(n + 1);
+	vl f(n + 1), can(n + 1);
+	dp[1].set(n);
+	for (int i = 1; i <= n; ++i) {
+		ll mask = 0, zero = 0;
+		for (int j = n; j >= i; --j) {
+			ll x = sum[j] ^ sum[i - 1];
+			int h = 63 - countl_zero(static_cast<ull>(x));
+			if (!dp[i].test(j)) {
+				if (zero) dp[i].set(j);
+				else {
+					if (mask & x || f[j] & x || can[j]) dp[i].set(j);
+				}
+			}
+			if (dp[i].test(j)) {
+				if (x) f[j] |= 1ll << h;
+				else can[j] = 1;
+				if (x == 0) zero = 1;
+				else mask |= 1ll << h;
+			}
+		}
+	}
+	for (int i = 1; i <= n; ++i) cout << (dp[i].test(i) ? 1 : 0);
+	cout << '\n';
+}
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+}
