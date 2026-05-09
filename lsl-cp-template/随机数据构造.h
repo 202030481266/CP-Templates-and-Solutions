6c6568960aaddb778300c4ef26ac2e478223a462
xiao_shulin <1258493030@qq.com>
2026-05-10 04:36:35 +0800

    [add] drink tea and modify the template

---
+++ lsl-cp-template/随机数据构造.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <vector>
+#include <random>
+#include <functional>
+
+template <typename T>
+std::vector<T> get_random_data(size_t times, T lower, T upper) {
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+
+	std::vector<T> res;
+	res.reserve(times);
+
+	if constexpr (std::is_integral_v<T>) {
+		std::uniform_int_distribution<T> dist(lower, upper);
+		for (size_t i = 0; i < times; ++i) res.push_back(dist(gen));
+	}
+	else {
+		std::uniform_real_distribution<T> dist(lower, upper);
+		for (size_t i = 0; i < times; ++i) res.push_back(dist(gen));
+	}
+	
+	return res;
+}
+
+template <typename T, typename Generator>
+std::vector<T> get_custom_data(size_t times, Generator gen_func) {
+	std::vector<T> res;
+	res.reserve(times);
+
+	for (size_t i = 0; i < times; ++i) res.push_back(gen_func());
+
+	return res;
+}
---
+++ tea-0x3f/cf2400~3000/798D.cpp
@@ -0,0 +1,169 @@
+// Problem: D. Mike and distribution
+// Contest: Codeforces - Codeforces Round 410 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/798/D
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#ifdef _MSC_VER
+#include "pch.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+#include <format>       // std::format
+#include <concepts>     // concepts
+#include <ranges>       // std::ranges
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using uint = unsigned int;
+using ld = long double;
+
+template<typename T, typename U = T>
+using pr = pair<T, U>;
+
+template<typename T>
+using vec = vector<T>;
+
+using pii = pr<int>;
+using pll = pr<ll>;
+using vi = vec<int>;
+using vl = vec<ll>;
+using vii = vec<vi>;
+using vll = vec<vl>;
+using vp = vec<pii>;
+using vpp = vec<vp>;
+
+template<typename T>
+concept Printable = requires(T x) { cerr << x; };
+
+template<typename T>
+concept PairLike = requires(T x) { x.first; x.second; };
+
+template<typename T>
+concept Iterable = ranges::range<T> && !PairLike<T> && !is_convertible_v<T, string_view>;
+
+template<typename T> void __print(const T&);
+
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+void __print(const char* x) { cerr << '"' << x << '"'; }
+void __print(const string& x) { cerr << '"' << x << '"'; }
+void __print(string_view x) { cerr << '"' << x << '"'; }
+template<Printable T>
+	requires (!PairLike<T> && !Iterable<T>
+&& !is_same_v<T, char>
+&& !is_same_v<T, bool>
+&& !is_same_v<T, string>
+&& !is_same_v<T, string_view>
+&& !is_convertible_v<T, const char*>)
+void __print(const T& x) { cerr << x; }
+
+template<PairLike T>
+void __print(const T& x) {
+	cerr << '{';
+	__print(x.first);
+	cerr << ", ";
+	__print(x.second);
+	cerr << '}';
+}
+
+template<typename... Ts>
+void __print(const tuple<Ts...>& t) {
+	cerr << '(';
+	apply([&]<typename... Args>(const Args&... args) {
+		size_t n = 0;
+		((cerr << (n++ ? ", " : ""), __print(args)), ...);
+	}, t);
+	cerr << ')';
+}
+
+template<Iterable T>
+void __print(const T& x) {
+	cerr << '{';
+	bool first = true;
+	for (const auto& i : x) {
+		if (!first) cerr << ", ";
+		__print(i);
+		first = false;
+	}
+	cerr << '}';
+}
+
+template<typename T>
+void __print(const optional<T>& x) {
+	if (x) { cerr << "Some("; __print(*x); cerr << ')'; }
+	else     cerr << "None";
+}
+
+void _print() { cerr << "]\n"; }
+
+template<typename T, typename... V>
+void _print(T&& t, V&&... v) {
+	__print(t);
+	if constexpr (sizeof...(v) > 0) cerr << ", ";
+	_print(forward<V>(v)...);
+}
+
+#ifndef ONLINE_JUDGE
+#   define debug(...) \
+        cerr << std::format("[{}:{}] [{}] = [", __FILE_NAME__, __LINE__, #__VA_ARGS__); \
+        _print(__VA_ARGS__)
+#else
+#   define debug(...)
+#endif
+
+inline constexpr int  MAXN = 100005;
+inline constexpr int  INF = 0x3f3f3f3f;
+inline constexpr ll   LLINF = 0x3f3f3f3f3f3f3f3fLL;
+inline constexpr int  MOD = 1'000'000'007;
+inline constexpr int  MAXB = 30;
+
+inline constexpr int  INT_INF = numeric_limits<int>::max() / 2;
+inline constexpr ll   LL_INF = numeric_limits<ll>::max() / 2;
+
+
+void solve() {
+	int n;
+	cin >> n;
+	vi a(n), b(n);
+	for (int& v : a) cin >> v;
+	for (int& v : b) cin >> v;
+	vi ids(n);
+	iota(ids.begin(), ids.end(), 0);
+	ranges::sort(ids, [&](int i, int j) { return a[i] == a[j] ? b[i] < b[j] : a[i] < a[j]; });
+	vi ans;
+	vi use(n);
+	for (int i = 0; i + 1 < n; i += 2) {
+		if (b[ids[i]] > b[ids[i + 1]]) {
+			ans.push_back(ids[i]);
+			use[i] = 1;
+		}
+		else {
+			ans.push_back(ids[i + 1]);
+			use[i + 1] = 1;
+		}
+	}
+	if (n & 1) {
+		ans.push_back(ids[n - 1]);
+	}
+	else {
+		if (!use[n - 1]) ans.push_back(ids[n - 1]);
+		else ans.push_back(ids[n - 2]);
+	}
+	cout << ans.size() << '\n';
+	for (int v : ans) cout << v + 1 << ' ';
+	cout << '\n';
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t = 1;
+	while (t--) solve();
+	return 0;
+}
