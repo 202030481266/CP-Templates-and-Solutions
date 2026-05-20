e8decc3cd7dce5b090a6590606ed63a4360fd8ee
xiao_shulin <1258493030@qq.com>
2026-05-21 00:50:23 +0800

    [add] add hard constructive problem and tea

---
+++ tea-0x3f/cf2400~3000/1019C.cpp
@@ -0,0 +1,156 @@
+#ifdef _MSC_VER
+#include "pch.h"
+#else
+#include <bits/stdc++.h>
+#endif
+#include <format>
+#include <concepts>
+#include <ranges>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using uint = unsigned int;
+using ld = long double;
+
+template<typename T, typename U = T>
+using pr = pair<T, U>;
+template<typename T>
+using vec = vector<T>;
+using pii = pr<int>;
+using pll = pr<ll>;
+using vi = vec<int>;
+using vl = vec<ll>;
+using vii = vec<vi>;
+using vll = vec<vl>;
+using vp = vec<pii>;
+using vpp = vec<vp>;
+
+template<typename T>
+concept Printable = requires(T x) { cerr << x; };
+template<typename T>
+concept PairLike = requires(T x) { x.first; x.second; };
+template<typename T>
+concept Iterable = ranges::range<T> && !PairLike<T> && !is_convertible_v<T, string_view>;
+
+template<typename T> void __print(const T&);
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+void __print(const char* x) { cerr << '"' << x << '"'; }
+void __print(const string& x) { cerr << '"' << x << '"'; }
+void __print(string_view x) { cerr << '"' << x << '"'; }
+
+template<Printable T>
+	requires (!PairLike<T> && !Iterable<T>
+&& !is_same_v<T, char>
+&& !is_same_v<T, bool>
+&& !is_same_v<T, string>
+&& !is_same_v<T, string_view>
+&& !is_convertible_v<T, const char*>)
+void __print(const T& x) { cerr << x; }
+
+template<PairLike T>
+void __print(const T& x) {
+	cerr << '{';
+	__print(x.first);
+	cerr << ", ";
+	__print(x.second);
+	cerr << '}';
+}
+
+template<typename... Ts>
+void __print(const tuple<Ts...>& t) {
+	cerr << '(';
+	apply([&]<typename... Args>(const Args&... args) {
+		size_t n = 0;
+		((cerr << (n++ ? ", " : ""), __print(args)), ...);
+	}, t);
+	cerr << ')';
+}
+
+template<Iterable T>
+void __print(const T& x) {
+	cerr << '{';
+	bool first = true;
+	for (const auto& i : x) {
+		if (!first) cerr << ", ";
+		__print(i);
+		first = false;
+	}
+	cerr << '}';
+}
+
+template<typename T>
+void __print(const optional<T>& x) {
+	if (x) { cerr << "Some("; __print(*x); cerr << ')'; }
+	else     cerr << "None";
+}
+
+void _print() { cerr << "]\n"; }
+template<typename T, typename... V>
+void _print(T&& t, V&&... v) {
+	__print(t);
+	if constexpr (sizeof...(v) > 0) cerr << ", ";
+	_print(forward<V>(v)...);
+}
+
+#ifndef ONLINE_JUDGE
+#   define debug(...) \
+        cerr << std::format("[{}:{}] [{}] = [", __FILE_NAME__, __LINE__, #__VA_ARGS__); \
+        _print(__VA_ARGS__)
+#else
+#   define debug(...)
+#endif
+
+inline constexpr int  MAXN = 300005;
+inline constexpr int  INF = 0x3f3f3f3f;
+inline constexpr ll   LLINF = 0x3f3f3f3f3f3f3f3fLL;
+inline constexpr int  MOD = 1'000'000'007;
+inline constexpr int  MAXB = 30;
+
+
+// 1e6, 2 seconds
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+
+	vii g(n + 1);
+	for (int i = 0, u, v; i < m; ++i) {
+		cin >> u >> v;
+		g[u].push_back(v);
+	}
+
+	vi vis(n + 1);
+	vi arr;
+	for (int i = 1; i <= n; ++i) {
+		if (!vis[i]) {
+			vis[i] = 1;
+			arr.push_back(i);
+			for (int v : g[i]) vis[v] = 1;
+		}
+	}
+	
+	int N = arr.size();
+	vi ans;
+	for (int v : arr) vis[v] = 0;
+	for (int i = N - 1; i >= 0; --i) {
+		if (!vis[arr[i]]) {
+			vis[arr[i]] = 1;
+			ans.push_back(arr[i]);
+			for (int v : g[arr[i]]) vis[v] = 1;
+		}
+	}
+	
+	cout << ans.size() << '\n';
+	for (int v : ans) cout << v << ' ';
+}
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t = 1;
+	while (t--) solve();
+	return 0;
+}
