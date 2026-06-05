6021976106dd74ac5b67471ee9b61335a404075b
xiao_shulin <1258493030@qq.com>
2026-06-06 00:13:11 +0800

    [add] drink tea

---
+++ tea-0x3f/2026/5月份/abc450_f.cpp
@@ -0,0 +1,165 @@
+#ifdef _MSC_VER
+#include "pch.h"
+#else
+#include <bits/stdc++.h>
+#endif
+#include <format>
+#include <concepts>
+#include <ranges>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using uint = unsigned int;
+using ld = long double;
+
+template<typename T, typename U = T>
+using pr = pair<T, U>;
+template<typename T>
+using vec = vector<T>;
+using pii = pr<int>;
+using pll = pr<ll>;
+using vi = vec<int>;
+using vl = vec<ll>;
+using vii = vec<vi>;
+using vll = vec<vl>;
+using vp = vec<pii>;
+using vpp = vec<vp>;
+
+template<typename T>
+concept Printable = requires(T x) { cerr << x; };
+template<typename T>
+concept PairLike = requires(T x) { x.first; x.second; };
+template<typename T>
+concept Iterable = ranges::range<T> && !PairLike<T> && !is_convertible_v<T, string_view>;
+
+template<typename T> void __print(const T&);
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+void __print(const char* x) { cerr << '"' << x << '"'; }
+void __print(const string& x) { cerr << '"' << x << '"'; }
+void __print(string_view x) { cerr << '"' << x << '"'; }
+
+template<Printable T>
+	requires (!PairLike<T> && !Iterable<T>
+&& !is_same_v<T, char>
+&& !is_same_v<T, bool>
+&& !is_same_v<T, string>
+&& !is_same_v<T, string_view>
+&& !is_convertible_v<T, const char*>)
+void __print(const T& x) { cerr << x; }
+
+template<PairLike T>
+void __print(const T& x) {
+	cerr << '{';
+	__print(x.first);
+	cerr << ", ";
+	__print(x.second);
+	cerr << '}';
+}
+
+template<typename... Ts>
+void __print(const tuple<Ts...>& t) {
+	cerr << '(';
+	apply([&]<typename... Args>(const Args&... args) {
+		size_t n = 0;
+		((cerr << (n++ ? ", " : ""), __print(args)), ...);
+	}, t);
+	cerr << ')';
+}
+
+template<Iterable T>
+void __print(const T& x) {
+	cerr << '{';
+	bool first = true;
+	for (const auto& i : x) {
+		if (!first) cerr << ", ";
+		__print(i);
+		first = false;
+	}
+	cerr << '}';
+}
+
+template<typename T>
+void __print(const optional<T>& x) {
+	if (x) { cerr << "Some("; __print(*x); cerr << ')'; }
+	else     cerr << "None";
+}
+
+void _print() { cerr << "]\n"; }
+template<typename T, typename... V>
+void _print(T&& t, V&&... v) {
+	__print(t);
+	if constexpr (sizeof...(v) > 0) cerr << ", ";
+	_print(forward<V>(v)...);
+}
+
+#ifndef ONLINE_JUDGE
+#   define debug(...) \
+        cerr << std::format("[{}:{}] [{}] = [", __FILE_NAME__, __LINE__, #__VA_ARGS__); \
+        _print(__VA_ARGS__)
+#else
+#   define debug(...)
+#endif
+
+inline constexpr int  MAXN = 300005;
+inline constexpr int  INF = 0x3f3f3f3f;
+inline constexpr ll   LLINF = 0x3f3f3f3f3f3f3f3fLL;
+inline constexpr int  MOD_197 = 1'000'000'007;
+inline constexpr int  MOD_998 = 998'244'353;
+inline constexpr int  MAXB = 30;
+
+
+#include <atcoder/lazysegtree>
+#include <atcoder/modint>
+
+using namespace std;
+using namespace atcoder;
+
+using mint = modint998244353;
+
+using S = mint;
+using F = mint;
+
+S op(S a, S b) { return a + b; }
+
+S e() { return 0; }
+
+S mapping(F f, S x) { return f * x; }
+
+F composition(F f, F g) { return f * g; }
+
+F id() { return 1; }
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<pair<int, int>> intervals(m);
+	for (auto& [r, l] : intervals) {
+		cin >> l >> r;
+	}
+	ranges::sort(intervals);
+
+	lazy_segtree<S, op, e, F, mapping, composition, id> dp(n + 1);
+
+	dp.set(1, 1);
+	for (auto [r, l] : intervals) {
+		mint s = dp.prod(l, r + 1);
+		dp.set(r, dp.get(r) + s);
+		if (l > 1) {
+			dp.apply(1, l, 2);
+		}
+	}
+
+	cout << dp.get(n).val() << '\n';
+}
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t = 1;
+	while (t--) solve();
+	return 0;
+}
